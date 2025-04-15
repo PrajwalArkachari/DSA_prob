60b41050e89ebb224a646e33ca610327468e1383
Prajwal <prajwal@scita.in>
2025-04-15 11:50:13 +0530

    1st day of coding

---
+++ DSA_prob/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Demo
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Enter the number to count the digit");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("The number "+n+" has "+countDigit(n)+" digits");
+        
+    }
+
+    public static int countDigit(int n)
+    {
+        int count = 0;
+        while(n > 0)
+        {
+            count++;
+            n = n / 10;
+        }
+        return count;
+    }
+}
