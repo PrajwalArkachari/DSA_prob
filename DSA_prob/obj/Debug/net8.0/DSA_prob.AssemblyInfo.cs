95d6097f9360291c45cf0265875422796595c1a5
Prajwal <prajwal@scita.in>
2025-04-15 12:31:10 +0530

    Structured within the folder

--- DSA_prob/obj/Debug/net8.0/DSA_prob.AssemblyInfo.cs
+++ DSA_prob/obj/Debug/net8.0/DSA_prob.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DSA_prob")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60b41050e89ebb224a646e33ca610327468e1383")]
 [assembly: System.Reflection.AssemblyProductAttribute("DSA_prob")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DSA_prob")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
