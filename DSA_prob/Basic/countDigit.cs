07f8359747ffd858c32c915eeeb139eb42349e9e
Prajwal <prajwal@scita.in>
2025-04-15 14:31:46 +0530

    Day2:Reverse the given number

--- DSA_prob/Basic/countDigit.cs
+++ DSA_prob/Basic/countDigit.cs
@@ -1,30 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace DSA_prob.Basic
-{
-    internal class demo
-    {
-        public static void Main(String[] args)
-        {
-            Console.WriteLine("Enter the number to count the digit");
-            int n = int.Parse(Console.ReadLine());
-            Console.WriteLine("The number " + n + " has " + countDigit(n) + " digits");
+//namespace DSA_prob.Basic
+//{
+//    internal class demo
+//    {
+//        public static void Main(String[] args)
+//        {
+//            Console.WriteLine("Enter the number to count the digit");
+//            int n = int.Parse(Console.ReadLine());
+//            Console.WriteLine("The number " + n + " has " + countDigit(n) + " digits");
 
-        }
+//        }
 
-        public static int countDigit(int n)
-        {
-            int count = 0;
-            while (n > 0)
-            {
-                count++;
-                n = n / 10;
-            }
-            return count;
-        }
-    }
-}
+//        public static int countDigit(int n)
+//        {
+//            int count = (int)Math.Log10(n) + 1;
+//            //while (n > 0)
+//            //{
+//            //    count++;
+//            //    n = n / 10;
+//            //}
+//            return count;
+//        }
+//    }
+//}
---
+++ DSA_prob/Basic/reverseNum.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace DSA_prob.Basic
+//{
+//    internal class reverseNumber
+//    {
+//        static void Main(string[] args)
+//        {
+//            int num = int.Parse(Console.ReadLine());
+//            Console.WriteLine(reverseNum(num));
+//        }
+//        static int reverseNum(int num)
+//        {
+//            int rev = 0;
+//            while(num > 0)
+//            {
+//                rev = rev * 10 + num%10;
+//                num /= 10;
+//            }
+//            return rev;
+//        }
+//    }
+//}
