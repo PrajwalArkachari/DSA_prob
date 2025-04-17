8b47756d64e9e6363d1c87e3b6c9682d0c3f7033
Prajwal <prajwal@scita.in>
2025-04-17 18:00:29 +0530

    check the string is palindrome is not

---
+++ DSA_prob/Basic/checkStringPalindrome.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace DSA_prob.Basic
+//{
+//    internal class checkStringPalindrome
+//    {
+//        static void Main(string[] args)
+//        {
+//            string str = Console.ReadLine();
+//            if (checkPalindrome(str)) Console.WriteLine("String is Palindrome");
+//            else Console.WriteLine("String is not Palindrome");
+//        }
+//        static bool checkPalindrome(string str)
+//        {
+//            int left=0, right=str.Length-1;
+//            while(left<right)
+//            {
+//                if (str[left] != str[right])
+//                {
+//                    return false;
+                    
+//                }
+//                left++; right--;
+//            }
+//            return true;
+//        }
+//    }
+//}
