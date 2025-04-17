46ef11e946a6e313532fd82d8eaf14473db124d8
Prajwal <prajwal@scita.in>
2025-04-17 16:06:57 +0530

    sum of 1st n numbers

--- DSA_prob/Basic/Recursive1toN.cs
+++ DSA_prob/Basic/Recursive1toN.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace DSA_prob.Basic
-{
-    internal class Recursive1toN
-    {
-        static void Main(string[] args)
-        {
-            int n = int.Parse(Console.ReadLine());
-            printN(1,n);
-        }
-        static void printN(int i,int n)
-        {
-            if(i > n)
-            {
-                return;
-            }
-            Console.WriteLine(i);
-            printN(i+1,n);
+//namespace DSA_prob.Basic
+//{
+//    internal class Recursive1toN
+//    {
+//        static void Main(string[] args)
+//        {
+//            int n = int.Parse(Console.ReadLine());
+//            printN(1,n);
+//        }
+//        static void printN(int i,int n)
+//        {
+//            if(i > n)
+//            {
+//                return;
+//            }
+//            Console.WriteLine(i);
+//            printN(i+1,n);
 
-        }
-    }
-}
+//        }
+//    }
+//}
