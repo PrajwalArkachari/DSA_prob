97e99eb8db5dd35dfe1ea7adce8456f8ab7b03fa
Prajwal <prajwal@scita.in>
2025-04-17 14:49:51 +0530

    recursive coding

---
+++ DSA_prob/Basic/Recursive1toN.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_prob.Basic
+{
+    internal class Recursive1toN
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            printN(1,n);
+        }
+        static void printN(int i,int n)
+        {
+            if(i > n)
+            {
+                return;
+            }
+            Console.WriteLine(i);
+            printN(i+1,n);
+
+        }
+    }
+}
