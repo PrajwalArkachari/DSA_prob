325e805c475699b19e2d1789f6726a20c829eff1
Prajwal <prajwal@scita.in>
2025-05-06 16:35:39 +0530

    max consiquitive array

--- DSA_prob/Array/Easy/missingNumber.cs
+++ DSA_prob/Array/Easy/missingNumber.cs
@@ -1,29 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace DSA_prob.Array.Easy
-{
-    internal class missingNumber
-    {
-        static void Main(string[] args)
-        {
-            int n = int.Parse(Console.ReadLine());
-            int[] arr = new int[n];
-            for(int i = 0; i < n; i++) arr[i] = int.Parse(Console.ReadLine());
-            int missing = MissingNumber(arr);
-            Console.WriteLine(missing);
-        }
-        static int MissingNumber(int[] nums)
-        {
-            int n = nums.Length;
-            int sum = (n * (n + 1)) / 2;
-            int j = 0;
-            for (int i = 0; i < n; i++) j += nums[i];
-            int missingNum = sum - j;
-            return missingNum;
-        }
-    }
-}
+//namespace DSA_prob.Array.Easy
+//{
+//    internal class missingNumber
+//    {
+//        static void Main(string[] args)
+//        {
+//            int n = int.Parse(Console.ReadLine());
+//            int[] arr = new int[n];
+//            for(int i = 0; i < n; i++) arr[i] = int.Parse(Console.ReadLine());
+//            int missing = MissingNumber(arr);
+//            Console.WriteLine(missing);
+//        }
+//        static int MissingNumber(int[] nums)
+//        {
+//            int n = nums.Length;
+//            int sum = (n * (n + 1)) / 2;
+//            int j = 0;
+//            for (int i = 0; i < n; i++) j += nums[i];
+//            int missingNum = sum - j;
+//            return missingNum;
+//        }
+//    }
+//}
