9776c4be9175e3b925df3742bfd226401e091418
Prajwal <prajwal@scita.in>
2025-05-06 17:28:04 +0530

    appears onces arrays --> easy..

--- DSA_prob/Array/Easy/appearsOnce.cs
+++ DSA_prob/Array/Easy/appearsOnce.cs
@@ -6,32 +6,25 @@
 
 //namespace DSA_prob.Array.Easy
 //{
-//    internal class maxConsiquitiveEle
+//    internal class appearsOnce
 //    {
 //        static void Main(string[] args)
 //        {
 //            int n = int.Parse(Console.ReadLine());
 //            int[] arr = new int[n];
 //            for (int i = 0; i < n; i++) arr[i] = int.Parse(Console.ReadLine());
-//            int max = FindMaxConsecutiveOnes(arr);
-//            Console.WriteLine("Maximum element is "+max);
+//            int number=SingleNumber(arr);
+//            Console.WriteLine("Number appears only onces in the given array is "+number);
+
 //        }
-//        public static int FindMaxConsecutiveOnes(int[] nums)
+//        public static int SingleNumber(int[] nums)
 //        {
-//            int max = 0;
-//            int count = 0;
+//            int xor = 0;
 //            for (int i = 0; i < nums.Length; i++)
 //            {
-//                if (nums[i] == 1)
-//                {
-//                    count++;
-//                    max = Math.Max(max, count);
-//                }
-//                if (nums[i] == 0) count = 0;
-
+//                xor = xor ^ nums[i];
 //            }
-//            return max;
+//            return xor;
 //        }
 //    }
-
 //}
